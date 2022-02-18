5910f3079e10230aed3837e5d89ee07c0a65709e
Burak Durak <bhdurak@gmail.com>
2022-02-18 20:02:13 +0300

    middlewares

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const app = express()
 
 app.use(express.json())
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+app.use(requestLogger)
+
 let persons = [
     { 
       "id": 1,
@@ -86,6 +95,12 @@ app.post('/api/persons', (request, response) => {
     response.status(400).json({error:'Request Body not provided!'})
   }
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({error: 'Endpoint not found'})
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 
