5f2bb27dc55df5fbca97287855a248059fde8d0b
Burak Durak <bhdurak@gmail.com>
2022-02-24 13:09:37 +0300

    Exercise 3.9

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express()
 app.use(express.json())
 morgan.token('body', (request, response) => JSON.stringify(request.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('build'))
 
 
 
